2e503d0a4b9dff88829082f5517691a9df5aacd0
figoperdana <192410102022@mail.unej.ac.id>
2023-03-09 17:49:47 +0700

    update

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	word := "selamat malam"
+	chars := (word)
+	charCount := make(map[string]int)
+
+	
+	for _, char := range word {
+		charCount[string(char)]++
+	}
+
+	for i := 0; i < len(chars); 
+	i++ {
+		char := string(chars[i])
+		println(char)
+	}
+	fmt.Println(charCount)
+}
